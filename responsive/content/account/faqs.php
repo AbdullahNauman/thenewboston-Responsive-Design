b1702a2aa643816f9283eb80a701a31ee736563d
Bucky Roberts <buckyroberts@gmail.com>
2015-07-19 10:20:28 -0400

    Worked on FAQ's page.

--- responsive/content/account/faqs.php
+++ responsive/content/account/faqs.php
@@ -7,7 +7,9 @@
 
 	<title>FAQ's</title>
 
-	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin" rel="stylesheet" type="text/css">
+	<link
+		href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin"
+		rel="stylesheet" type="text/css">
 	<link href="../../assets/stylesheets/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="../../assets/stylesheets/pixel-admin.min.css" rel="stylesheet" type="text/css">
 	<link href="../../assets/stylesheets/widgets.min.css" rel="stylesheet" type="text/css">
@@ -33,6 +35,58 @@
 			<div class="row">
 				<h1 class="col-xs-12 col-sm-4 text-center text-left-sm">FAQ's</h1>
 			</div>
+		</div>
+
+		<div class="row">
+
+			<div class="col-md-8">
+				<div class="panel colourable">
+					<div class="panel-body">
+
+						<h2>General</h2>
+
+						<hr>
+
+						<h4>WTF is thenewboston?</h4>
+						<p>Well, thenewboston is a social network that was built for computer programmers, web designers, and tech enthusiast. However, anyone is welcome to join.</p><br>
+
+						<h4>How do I upload a profile picture?</h4>
+						<p>When posting a photo, select the option "Use as Profile Photo" at the bottom. Additionally, you may visit your Manage Photos page after uploading a photo and click the "Set as Profile Photo" link.</p><br>
+
+						<h4>Why didn't I receive a verification email?</h4>
+						<p>Clam down Hoss. Check your Spam folder. Also, some users are reporting that certain email providers (such as Yahoo!) are blocking emails from thenewboston. If you do not receive a verification email within a few hours after registration, please use a different email provider.</p><br>
+
+						<h4>What are credits?</h4>
+						<p>Credits can be used to list items in the Trade section or the Shop section of the website.</p><br>
+
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="panel colourable">
+					<div class="panel-body">
+
+						<h4>General</h4>
+						<ul style="list-style-type:none; padding-left:0;">
+							<li><a href="#">WTF is thenewboston?</a></li>
+							<li><a href="#">How do I upload a profile picture?</a></li>
+							<li><a href="#">Why didn't I receive a verification email?</a></li>
+							<li><a href="#">What are credits?</a></li>
+						</ul>
+
+						<h4>Forum</h4>
+						<ul style="list-style-type:none; padding-left:0;">
+							<li><a href="#">WTF is thenewboston?</a></li>
+							<li><a href="#">How do I upload a profile picture?</a></li>
+							<li><a href="#">Why didn't I receive a verification email?</a></li>
+							<li><a href="#">What are credits?</a></li>
+						</ul>
+
+					</div>
+				</div>
+			</div>
+
 		</div>
 
 	</div>
