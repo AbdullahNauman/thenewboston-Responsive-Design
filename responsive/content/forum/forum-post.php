b08fed83d97752d164a52e66df81df327707f6a1
Abdullah Nauman <clubwriters992@gmail.com>
2015-07-22 19:54:29 -0400

    the new forum layout

---
+++ responsive/content/forum/forum-post.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Forum</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin" rel="stylesheet" type="text/css">
+	<link href="../../assets/stylesheets/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="../../assets/stylesheets/pixel-admin.min.css" rel="stylesheet" type="text/css">
+	<link href="../../assets/stylesheets/widgets.min.css" rel="stylesheet" type="text/css">
+	<link href="../../assets/stylesheets/pages.min.css" rel="stylesheet" type="text/css">
+	<link href="../../assets/stylesheets/themes.min.css" rel="stylesheet" type="text/css">   
+	<script src = "https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<link rel = "stylesheet" href = "style.css"> 
+
+
+ 
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body class="theme-default main-menu-animated">
+<script>var init = [];</script>
+<div id="main-wrapper">
+	<?php include("../../panel/top_nav.php"); ?>
+	<?php include("../../panel/left_forum.php"); ?>
+
+	<div id="content-wrapper">
+
+		<!-- Page header -->
+		<div class="page-header">
+			<div class="row">
+				<h1 class="col-xs-12 col-sm-4 text-center text-left-sm"> Web Development </h1>
+				<input type="text" class="form-control" id="search" placeholder = "Search Topics, Questions, ..">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<p> 
+     			
+     			     <div class = "sidebar">  
+
+     			     	<img src = "https://www.thenewboston.com/photos/users/9212/resized/bebd94b4921de72ba46cb24cd936a9e1.png" class = "profilePicForumPost"> 
+     			     	 <font size = "4">Tim Bogdanov | +13 </font><i class="fa fa-heart-o fa-2x"></i> </p>
+
+
+
+     			        </div>
+     			     </div> 
+
+
+     			     <div class = "forum-content">   
+
+     			     	<h3><span class = "forum-post-content"> I need some help with the positioning of my div elements. </h3> 
+
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lacus tristique, cursus turpis sit amet, euismod felis. Sed in mauris non felis commodo rhoncus sit amet ac dolor. Etiam elit mi, imperdiet non neque at, tincidunt commodo erat. Curabitur sit amet lorem a orci suscipit ultrices. Sed aliquam at risus sed tempor. Sed nunc sapien, tempus in vulputate eget, imperdiet luctus neque. Pellentesque ipsum elit, vestibulum sed nisl eu, vestibulum placerat eros. In bibendum posuere neque ut bibendum. Aliquam malesuada arcu sed urna tempus, eu dictum ligula faucibus. Donec aliquam odio ligula, vel pharetra mauris bibendum a. Mauris mauris elit, volutpat a leo at, placerat tristique tellus.
+
+                                Etiam in porta felis. Duis suscipit velit id sodales rutrum. Etiam vehicula et dui eu accumsan. Mauris convallis tincidunt iaculis. Vivamus faucibus quam vitae nibh rutrum pellentesque. Phasellus quis diam diam. Aliquam suscipit justo in elit ultrices finibus. Sed non venenatis urna. Sed eu justo vestibulum metus auctor scelerisque. Quisque iaculis erat id elit tristique, consectetur vestibulum tortor tempus.
+
+                                ur sollicitudin ex. Praesent nisl metus, tempor gravida posuere sed, condimentum nec m
+								
+                                </span>
+
+
+     			     	 </span>
+     			     </div> 
+   					<br>
+     			     <hr> 
+     			    <div class = "sidebar">  
+
+     			     	<img src = "https://www.thenewboston.com/photos/users/553/resized/36273a633c257e6bdfb7ab5854544036.jpg" class = "profilePicForumPost"> 
+     			     	 <font size = "4">Lanchan Chant |  <a class="btn btn-success btn-sm" href="#"> <i class="fa fa-check"></i> Best Answer </a> </font></p>
+
+
+     			        </div>
+     			     
+
+
+     			     <div class = "forum-content">   
+
+     			     <br> 
+
+     			     		
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lacus tristique, cursus turpis sit amet, euismod felis. Sed in mauris non felis commodo rhoncus sit amet ac dolor. Etiam elit mi, imperdiet non neque at, tincidunt commodo erat. Curabitur sit amet lorem a orci suscipit ultrices. Sed aliquam at risus sed tempor. Sed nunc sapien, tempus in vulputate eget, imperdiet luctus neque. Pellentesque ipsum elit, vestibulum sed nisl eu, vestibulum placerat eros. In bibendum posuere neque ut bibendum. Aliquam malesuada arcu sed urna tempus, eu dictum ligula faucibus. Donec aliquam odio ligula, vel pharetra mauris bibendum a. Mauris mauris elit, volutpat a leo at, placerat tristique tellus.
+
+                                    Etiam in porta felis. Duis suscipit velit id sodales rutrum. Etiam vehicula et dui eu accumsan. Mauris convallis tincidunt iaculis. Vivamus faucibus quam vitae nibh rutrum pellentesque. Phasellus quis diam diam. Aliquam suscipit justo in elit ultrices finibus. Sed non venenatis urna. Sed eu justo vestibulum metus auctor scelerisque. Quisque iaculis erat id elit tristique, consectetur vestibulum tortor tempus.
+
+                                    ur sollicitudin ex. Praesent nisl metus, tempor gravida posuere sed, condimentum nec m
+								</span>
+
+
+     			     	 </span>
+
+
+
+                     </div>  
+                     <br>
+                     <hr>  
+
+                       <div class = "sidebar">  
+
+                        <img src = "https://www.thenewboston.com/photos/users/23/resized/42ace1f0a53e60de6ddce767ddfda9e9.jpg" class = "profilePicForumPost"> 
+                         <font size = "4">Pere Garau Burguera | +13 </font><i class="fa fa-heart-o fa-2x"></i> </p>
+
+
+
+                        </div>
+                     
+
+
+                     <div class = "forum-content">   
+
+                     <!-- h3><span class = "forum-post-content"> I need some help with the positioning of my div elements. </h3>  --><br> 
+
+                            
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lacus tristique, cursus turpis sit amet, euismod felis. Sed in mauris non felis commodo rhoncus sit amet ac dolor. Etiam elit mi, imperdiet non neque at, tincidunt commodo erat. Curabitur sit amet lorem a orci suscipit ultrices. Sed aliquam at risus sed tempor. Sed nunc sapien, tempus in vulputate eget, imperdiet luctus neque. Pellentesque ipsum elit, vestibulum sed nisl eu, vestibulum placerat eros. In bibendum posuere neque ut bibendum. Aliquam malesuada arcu sed urna tempus, eu dictum ligula faucibus. Donec aliquam odio ligula, vel pharetra mauris bibendum a. Mauris mauris elit, volutpat a leo at, placerat tristique tellus.
+
+                                        Etiam in porta felis. Duis suscipit velit id sodales rutrum. Etiam vehicula et dui eu accumsan. Mauris convallis tincidunt iaculis. Vivamus faucibus quam vitae nibh rutrum pellentesque. Phasellus quis diam diam. Aliquam suscipit justo in elit ultrices finibus. Sed non venenatis urna. Sed eu justo vestibulum metus auctor scelerisque. Quisque iaculis erat id elit tristique, consectetur vestibulum tortor tempus.
+
+                                        ur sollicitudin ex. Praesent nisl metus, tempor gravida posuere sed, condimentum nec m
+                                </span>
+
+
+                         </span>
+
+
+
+                     </div>
+      		</div>
+
+              <!-- Sorry, I don't know PHP. So I do not which location this command reffrences - Abdullah Nauman-->			
+					 <?php //echo(basename(__DIR__)); ?> 
+				</p>
+			</div>
+		</div>
+
+	</div>
+
+	<div id="main-menu-bg"></div>
+</div>
+<?php include("../../panel/end_scripts.php"); ?>
+</body>
+</html>
--- responsive/content/forum/specific-topic-forum.php
+++ responsive/content/forum/specific-topic-forum.php
@@ -6,18 +6,18 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>Forum</title>
-
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin" rel="stylesheet" type="text/css">
 	<link href="../../assets/stylesheets/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="../../assets/stylesheets/pixel-admin.min.css" rel="stylesheet" type="text/css">
 	<link href="../../assets/stylesheets/widgets.min.css" rel="stylesheet" type="text/css">
 	<link href="../../assets/stylesheets/pages.min.css" rel="stylesheet" type="text/css">
-	<link href="../../assets/stylesheets/themes.min.css" rel="stylesheet" type="text/css">
-	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
- 	<link rel = "stylesheet" href = "style.css">
+	<link href="../../assets/stylesheets/themes.min.css" rel="stylesheet" type="text/css"> 
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>   
+	<link rel = "stylesheet" href = "style.css"> 
 
 
-
+ 
 
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -35,19 +35,19 @@
 		<!-- Page header -->
 		<div class="page-header">
 			<div class="row">
-				<h1 class="col-xs-12 col-sm-4 text-center text-left-sm"> My Forum Feed</h1>
+				<h1 class="col-xs-12 col-sm-4 text-center text-left-sm"> Web Development </h1>
 				<input type="text" class="form-control" id="search" placeholder = "Search Topics, Questions, ..">
 			</div>
 		</div>
 
 		<div class="row">
 			<div class="col-xs-12">
-				<p>
-
+				<p> 
+     			
 				<div class = "post-container">
 				   <div class = "post">
 				   	<div id = "image-container">
-				   		<img src = "https://www.thenewboston.com/photos/users/2/resized/23471ba4417d650505928a0b1f1fd8b1.jpg" id = "profile-picture">
+				   		<img src = "https://www.thenewboston.com/photos/users/2/resized/23471ba4417d650505928a0b1f1fd8b1.jpg" id = "profile-picture"> 
 				   		<span id = "title"> <font size = "4"> &nbsp; &nbsp; General Forum Rules </font></span>
 				   	</div>
 				   	   <span class = "details">
@@ -56,62 +56,62 @@
 				   		<span id = "date"> Posted on 7/20/15 - </span>
 				   		<span id = "section">General Announcements -  </span>
 				   		<span id = "name"> Bucky Roberts - </span>
-				   		<sup><a class="btn btn-small btn-danger"> <i class="fa fa-microphone fa-lg"></i> Announcment</a></sup>
-
+				   		<sup><a class="btn btn-danger btn-sm"> <i class="fa fa-microphone fa-lg"></i> Announcment</a></sup> 
+				   		
 				   		</span>
 				   		<hr>
-				   </div>
-
+				   </div> 
+				   
 				   <div class = "post">
 				   	<div id = "image-container">
-				   		<img src = "https://www.thenewboston.com/photos/users/27/resized/d1dc02a41f61faca2687f08a7c32ea4c.jpg" id = "profile-picture">
+				   		<img src = "https://www.thenewboston.com/photos/users/27/resized/d1dc02a41f61faca2687f08a7c32ea4c.jpg" id = "profile-picture"> 
 				   		<span id = "title"> <font size = "4"> &nbsp; &nbsp; This is a sample post  </font></span>
 				   	</div>
 				   	   <span class = "details">
 				        <span id = "views">50 <i class="fa fa-eye fa-lg"></i> - </span>
 
 				   		<span id = "date"> Posted on 7/20/15 - </span>
-				   		<span id = "section">NodeJS -  </span>
+				   		<span id = "section">NodeJS -  </span> 
 				   		<span id = "name"> Wheatley Core </span>
 				   		<!-- <a class="btn btn-info"> <i class="fa fa-certificate fa-lg"></i> Moderator </a> -->
 				   		</span>
 				   		<hr>
-				   </div>
-
+				   </div>    
+				   	
 				   <div class = "post">
 				   	<div id = "image-container">
-				   		<img src = "https://www.thenewboston.com/photos/users/622/resized/7f41cbab0e3e6e047f59e0adb9e35e21.png" id = "profile-picture">
+				   		<img src = "https://www.thenewboston.com/photos/users/622/resized/7f41cbab0e3e6e047f59e0adb9e35e21.png" id = "profile-picture"> 
 				   		<span id = "title"> <font size = "4"> &nbsp; &nbsp; This is a sample post  </font></span>
 				   	</div>
 				   	   <span class = "details">
 				        <span id = "views">50 <i class="fa fa-eye fa-lg"></i> - </span>
 
 				   		<span id = "date"> Posted on 7/20/15 - </span>
-				   		<span id = "section">Andorid App Development -  </span>
+				   		<span id = "section">Andorid App Development -  </span> 
 				   		<span id = "name"> Samuel Oloruntoba</span>
-
+				   		
 				   		</span>
 				   		<hr>
-				   </div>
-
+				   </div> 
+				   
 				   <div class = "post">
 				   	<div id = "image-container">
-				   		<img src = "https://www.thenewboston.com/photos/users/2646/resized/62b6884bf4ac2d4b91ad6e97602d9b4b.jpg" id = "profile-picture">
+				   		<img src = "https://www.thenewboston.com/photos/users/2646/resized/62b6884bf4ac2d4b91ad6e97602d9b4b.jpg" id = "profile-picture"> 
 				   		<span id = "title"> <font size = "4"> &nbsp; &nbsp; This is a sample post  </font></span>
 				   	</div>
 				   	   <span class = "details">
 				        <span id = "views">50 <i class="fa fa-eye fa-lg"></i> - </span>
 
 				   		<span id = "date"> Posted on 7/20/15 - </span>
-				   		<span id = "section">Web Developement -  </span>
+				   		<span id = "section">Web Developement -  </span> 
 				   		<span id = "name"> Abdullah Nauman </span>
 				   		</span>
 				   		<hr>
-				   </div>
-
+				   </div> 
+				   
 				   <div class = "post">
 				   	<div id = "image-container">
-				   		<img src = "https://www.thenewboston.com/photos/users/23/resized/42ace1f0a53e60de6ddce767ddfda9e9.jpg" id = "profile-picture">
+				   		<img src = "https://www.thenewboston.com/photos/users/23/resized/42ace1f0a53e60de6ddce767ddfda9e9.jpg" id = "profile-picture"> 
 				   		<span id = "title"> <font size = "4"> &nbsp; &nbsp; This is a sample post  </font></span>
 				   	</div>
 				   	   <span class = "details">
@@ -119,54 +119,93 @@
 
 				   		<span id = "date"> Posted on 7/20/15 - </span>
 				   		<span id = "section">Java -  </span>
-				   		<!-- <a class="btn btn-info"> <i class="fa fa-certificate fa-lg"></i> Moderator </a> -->
+				   		<!-- <a class="btn btn-info"> <i class="fa fa-certificate fa-lg"></i> Moderator </a> --> 
 				   		<span id = "name"> Pere Garau Burguera </span>
 
 				   		</span>
 				   		<hr>
-				   </div>
-
+				   </div> 
+				   
 				   <div class = "post">
 				   	<div id = "image-container">
-				   		<img src = "https://www.thenewboston.com/photos/users/9212/resized/bebd94b4921de72ba46cb24cd936a9e1.png" id = "profile-picture">
+				   		<img src = "https://www.thenewboston.com/photos/users/9212/resized/bebd94b4921de72ba46cb24cd936a9e1.png" id = "profile-picture"> 
 				   		<span id = "title"> <font size = "4"> &nbsp; &nbsp; This is a sample post  </font></span>
 				   	</div>
 				   	   <span class = "details">
 				        <span id = "views">50 <i class="fa fa-eye fa-lg"></i> - </span>
 
 				   		<span id = "date"> Posted on 7/20/15 - </span>
-				   		<span id = "section">PHP -  </span>
+				   		<span id = "section">PHP -  </span> 
 				   		<span id = "name"> Tim Bogdanov  </span>
 				   		</span>
 				   		<hr>
-				   </div>
-
+				   </div> 
+				   
 				   <div class = "post">
 				   	<div id = "image-container">
-				   		<img src = "https://www.thenewboston.com/photos/users/302/resized/2d19b25e222ed4c82941b9f4be5d11aa.jpg" id = "profile-picture">
+				   		<img src = "https://www.thenewboston.com/photos/users/302/resized/2d19b25e222ed4c82941b9f4be5d11aa.jpg" id = "profile-picture"> 
 				   		<span id = "title"> <font size = "4"> &nbsp; &nbsp; This is a sample post  </font></span>
 				   	</div>
 				   	   <span class = "details">
 				        <span id = "views">50 <i class="fa fa-eye fa-lg"></i> - </span>
 
-				   		<span id = "date"> Posted on 7/20/15 - </span>
+				   		<span id = "date"> Posted on 7/20/15 - </span> 
 				   		<span id = "name"> Wheatley Core </span>
-				   		<span id = "section">Java -  </span>
+				   		<span id = "section">Java -  </span> 
 				   		<span id = "name"> Daniel Collier </span>
 				   		</span>
 				   		<hr>
-				   </div>
+				   </div> 
+				  
+
+				   </div> 
+				   
+
+      					<div id = "side-panel">
+      						<span class = "specific-topic-title">
+      						Web Development
+      						</span>  
+      						<hr> 
+      						<div class = "side-panel-image-container"> 
+
+      								<img src = "http://ecx.images-amazon.com/images/I/41KcvYbAEOL.png" class = "side-panel-icon">
+
+      						</div>  
+      						<div class = "side-panel-buttons">  
+
+      								<br>
+      								<a class="btn btn-danger" href="#">
+ 									<i class="fa fa-plus-square-o fa-lg"></i> Create new post </a> 
+ 									<a class="btn btn-info" href="#">
+ 									<i class="fa fa-user-plus fa-lg"></i> Follow Topic </a>
+
+      						</div>
+      						<div class = "side-panel-info">  
+                               
+		                            <span class = "specific-topic-title"> 
+		                            <hr>
+		                               About  
+		                            </span>   
+   										<hr>
+			                            <div class = "description">
+			                            	 HyperText Markup Language, commonly referred to as HTML, is the standard markup language used to create web pages. 
+			                            </div> 
+      									<br>
+			                          <div class = "moderators"> 
+                                          <h4> Moderators </h4>
+                                          <hr>
+			                            <img src = "https://www.thenewboston.com/photos/users/2/resized/23471ba4417d650505928a0b1f1fd8b1.jpg" class = "moderator-profile-picture"> &nbsp; &nbsp;
+                                        <img src = "https://www.thenewboston.com/photos/users/27/resized/d1dc02a41f61faca2687f08a7c32ea4c.jpg" class = "moderator-profile-picture"> &nbsp; &nbsp;
+ 										<img src = "https://www.thenewboston.com/photos/users/622/resized/7f41cbab0e3e6e047f59e0adb9e35e21.png" class = "moderator-profile-picture"> &nbsp; &nbsp;
+ 									  	<img src = "https://www.thenewboston.com/photos/users/9212/resized/bebd94b4921de72ba46cb24cd936a9e1.png" class = "moderator-profile-picture"> &nbsp; &nbsp;
+
+ 									  </div>
 
 
-				   </div>
+      					</div>
 
-               <!-- <div id = "container" style = "height: 1000px;">
-
-               		 <iframe src="TheNewBoston%20Forum/themesteam.com/cube/" height = "100%" width = "100%"></iframe>
-               </div> -->
-
-              <!-- Sorry, I don't know PHP. So I do not which location this command reffrences - Abdullah Nauman-->
-					 <?php //echo(basename(__DIR__)); ?>
+              <!-- Sorry, I don't know PHP. So I do not which location this command reffrences - Abdullah Nauman-->			
+					 <?php //echo(basename(__DIR__)); ?> 
 				</p>
 			</div>
 		</div>
